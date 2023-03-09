dfbffcb280dc36ddb4382eb10e351897dbf0668c
Emerson <emerson.day@gmail.com>
2023-03-08 17:53:35 -0800

    added shopify provider

--- app/root.jsx
+++ app/root.jsx
@@ -10,6 +10,15 @@ import tailwind from './styles/tailwind-build.css';
 import favicon from '../public/favicon.svg';
 import {Layout} from './components/Layout';
 import {Seo} from '@shopify/hydrogen';
+import {ShopifyProvider} from '@shopify/hydrogen-react';
+
+const shopifyConfig = {
+  storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
+  storeDomain: 'https://hydrogen-preview.myshopify.com',
+  storefrontApiVersion: '2023-01',
+  countryIsoCode: 'US',
+  languageIsoCode: 'en',
+};
 
 export const links = () => {
   return [
@@ -42,20 +51,22 @@ export default function App() {
   const {name} = data.layout.shop;
 
   return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-        <Seo />
-      </head>
-      <body>
-        <Layout title={name}>
-          <Outlet />
-        </Layout>
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
+    <ShopifyProvider {...shopifyConfig}>
+      <html lang="en">
+        <head>
+          <Meta />
+          <Links />
+          <Seo />
+        </head>
+        <body>
+          <Layout title={name}>
+            <Outlet />
+          </Layout>
+          <ScrollRestoration />
+          <Scripts />
+        </body>
+      </html>
+    </ShopifyProvider>
   );
 }
 
